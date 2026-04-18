2ba636fc5247d3b05fb56ff6a3ba0bcd54e322c2
Eliecer Andrei Ruiz Higuita <elieceruiz@me.com>
2026-04-18 02:08:35 -0500

    fix interrupcion + limpieza null + mongo config

--- db.py
+++ db.py
@@ -1,21 +1,11 @@
+import streamlit as st
 from pymongo import MongoClient
-import os
 
-# intentar primero Streamlit (cloud)
-try:
-    import streamlit as st
-    MONGO_URI = st.secrets["mongo_uri"]
-except:
-    # fallback local (.env)
-    from dotenv import load_dotenv
-    load_dotenv()
-    MONGO_URI = os.getenv("MONGO_URI")
-
-if not MONGO_URI:
-    raise ValueError("No se encontró MONGO_URI")
+MONGO_URI = st.secrets.get("MONGO_URI")
 
 client = MongoClient(MONGO_URI)
-
 db = client["registro_bucle"]
+
 coleccion_eventos = db["eventos"]
+coleccion_reflexiones = db["reflexiones"]
 coleccion_capital_b = db["capitalizacion_b"]
